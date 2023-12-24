865c8cb3433c694f8e02d4f5443f58deae851a4f
yaya_may <283631792@qq.com>
2023-12-24 19:15:28 +0800

    Create get_api.py

---
+++ get_api.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+import os
+import sys
+
+from typing import List
+
+from alibabacloud_sas20181203.client import Client as Sas20181203Client
+from alibabacloud_tea_openapi import models as open_api_models
+from alibabacloud_sas20181203 import models as sas_20181203_models
+from alibabacloud_tea_util import models as util_models
+from alibabacloud_tea_util.client import Client as UtilClient
+
+
+class Sample:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def create_client(
+        access_key_id: str,
+        access_key_secret: str,
+    ) -> Sas20181203Client:
+        """
+        使用AK&SK初始化账号Client
+        @param access_key_id:
+        @param access_key_secret:
+        @return: Client
+        @throws Exception
+        """
+        config = open_api_models.Config(
+            # 必填，您的 AccessKey ID,
+            access_key_id='LTAI5tH89KpFCPHjyeqh5vP3',
+            # 必填，您的 AccessKey Secret,
+            access_key_secret='M880ekA2YsVru91uWbUUpxvgNqrcLa'
+        )
+        # Endpoint 请参考 https://api.aliyun.com/product/Sas
+        config.endpoint = f'tds.aliyuncs.com'
+        return Sas20181203Client(config)
+
+    @staticmethod
+    def main(
+        args: List[str],
+    ) -> None:
+        # 请确保代码运行环境设置了环境变量 ALIBABA_CLOUD_ACCESS_KEY_ID 和 ALIBABA_CLOUD_ACCESS_KEY_SECRET。
+        # 工程代码泄露可能会导致 AccessKey 泄露，并威胁账号下所有资源的安全性。以下代码示例使用环境变量获取 AccessKey 的方式进行调用，仅供参考，建议使用更安全的 STS 方式，更多鉴权访问方式请参见：https://help.aliyun.com/document_detail/378659.html
+        client = Sample.create_client(os.environ['ALIBABA_CLOUD_ACCESS_KEY_ID'], os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET'])
+        list_instance_risk_levels_request = sas_20181203_models.ListInstanceRiskLevelsRequest()
+        runtime = util_models.RuntimeOptions()
+        try:
+            # 复制代码运行请自行打印 API 的返回值
+            client.list_instance_risk_levels_with_options(list_instance_risk_levels_request, runtime)
+        except Exception as error:
+            # 错误 message
+            print(error.message)
+            # 诊断地址
+            print(error.data.get("Recommend"))
+            UtilClient.assert_as_string(error.message)
+
+    @staticmethod
+    async def main_async(
+        args: List[str],
+    ) -> None:
+        # 请确保代码运行环境设置了环境变量 ALIBABA_CLOUD_ACCESS_KEY_ID 和 ALIBABA_CLOUD_ACCESS_KEY_SECRET。
+        # 工程代码泄露可能会导致 AccessKey 泄露，并威胁账号下所有资源的安全性。以下代码示例使用环境变量获取 AccessKey 的方式进行调用，仅供参考，建议使用更安全的 STS 方式，更多鉴权访问方式请参见：https://help.aliyun.com/document_detail/378659.html
+        client = Sample.create_client(os.environ['ALIBABA_CLOUD_ACCESS_KEY_ID'], os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET'])
+        list_instance_risk_levels_request = sas_20181203_models.ListInstanceRiskLevelsRequest()
+        runtime = util_models.RuntimeOptions()
+        try:
+            # 复制代码运行请自行打印 API 的返回值
+            await client.list_instance_risk_levels_with_options_async(list_instance_risk_levels_request, runtime)
+        except Exception as error:
+            # 错误 message
+            print(error.message)
+            # 诊断地址
+            print(error.data.get("Recommend"))
+            UtilClient.assert_as_string(error.message)
+
+
+if __name__ == '__main__':
+    Sample.main(sys.argv[1:])
